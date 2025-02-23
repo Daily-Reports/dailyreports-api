ab953a77d8ad0f03d76cbbd9c782edc6cd83204a
Thales Senra <thalesmarinhotwn@hotmail.com>
2025-02-22 21:51:06 -0300

    feat: created dto classes

---
+++ src/main/java/com/vasconcellos/dailyreport/dto/OrderDto.java
@@ -0,0 +1,42 @@
+package com.vasconcellos.dailyreport.dto;
+
+import com.vasconcellos.dailyreport.model.OrderSpeciality;
+import com.vasconcellos.dailyreport.model.OrderStatus;
+import com.vasconcellos.dailyreport.validation.NumberLength;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class OrderDto {
+
+    private Long id;
+
+    @NotNull
+    @NumberLength(length = 10)
+    private Long number;
+
+    @NotNull
+    private Long eventId;
+    @NotNull
+    private Long areaId;
+    private Long subareaId;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private OrderSpeciality speciality;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @NotBlank
+    @Size(min = 1, max = 100)
+    private String description;
+    @NotBlank
+    private String technical;
+
+}
