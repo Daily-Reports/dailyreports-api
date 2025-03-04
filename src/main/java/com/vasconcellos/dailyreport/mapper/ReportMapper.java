131851fbed28239ad572281d8ba8a2bf43d03021
Thales Senra <thalesmarinhotwn@hotmail.com>
2025-03-04 18:46:44 -0300

    feat: move validation from ReportMapper.java to ReportService.java

--- src/main/java/com/vasconcellos/dailyreport/mapper/ReportMapper.java
+++ src/main/java/com/vasconcellos/dailyreport/mapper/ReportMapper.java
@@ -1,13 +1,9 @@
 package com.vasconcellos.dailyreport.mapper;
 
 import com.vasconcellos.dailyreport.dto.ReportDto;
-import com.vasconcellos.dailyreport.exception.EmployeeNotFoundException;
-import com.vasconcellos.dailyreport.exception.InvalidEmployeeException;
 import com.vasconcellos.dailyreport.model.Employee;
-import com.vasconcellos.dailyreport.model.EmployeeType;
 import com.vasconcellos.dailyreport.model.Report;
 import com.vasconcellos.dailyreport.service.EmployeeService;
-import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
@@ -28,24 +24,8 @@ public abstract class ReportMapper {
     @Mapping(source = "supervisor.id", target = "supervisorId")
     public abstract ReportDto toDto(Report report);
 
-    @BeforeMapping
-    public void validatedReport(ReportDto data) {
-        validateEmployee(data.getForemanId(), EmployeeType.FOREMAN);
-        validateEmployee(data.getSupervisorId(), EmployeeType.SUPERVISOR);
-    }
-
     @Named("mapEmployeeById")
     public Employee mapEmployeeById(Long id) {
-        return employeeService.findById(id).orElse(null);
-    }
-
-    private void validateEmployee(Long employeeId, EmployeeType expectedType) {
-        Employee employee = mapEmployeeById(employeeId);
-
-        if (employee == null)
-            throw new EmployeeNotFoundException(expectedType.toString() + " cannot be found.");
-
-        if (employee.getType() != expectedType)
-            throw new InvalidEmployeeException("The specified " + expectedType.toString() + " has an invalid type.");
+        return employeeService.findById(id);
     }
 }
--- src/main/java/com/vasconcellos/dailyreport/service/ReportService.java
+++ src/main/java/com/vasconcellos/dailyreport/service/ReportService.java
@@ -1,7 +1,11 @@
 package com.vasconcellos.dailyreport.service;
 
 import com.vasconcellos.dailyreport.dto.ReportDto;
+import com.vasconcellos.dailyreport.exception.InvalidEmployeeException;
+import com.vasconcellos.dailyreport.exception.ResourceNotFoundException;
 import com.vasconcellos.dailyreport.mapper.ReportMapper;
+import com.vasconcellos.dailyreport.model.Employee;
+import com.vasconcellos.dailyreport.model.EmployeeType;
 import com.vasconcellos.dailyreport.model.Report;
 import com.vasconcellos.dailyreport.repository.ReportRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +13,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -22,11 +25,23 @@ public class ReportService {
     public Report save(ReportDto data) {
         Report toSave = reportMapper.toEntity(data);
 
+        validateEmployee(toSave.getForeman(), EmployeeType.FOREMAN);
+        validateEmployee(toSave.getForeman(), EmployeeType.SUPERVISOR);
+
         return reportRepository.save(toSave);
     }
 
-    public Optional<Report> findById(Long id) {
-        return reportRepository.findById(id);
+    private void validateEmployee(Employee employee, EmployeeType expectedType) {
+        if (employee == null)
+            throw new ResourceNotFoundException(expectedType.toString() + " cannot be found.");
+
+        if (employee.getType() != expectedType)
+            throw new InvalidEmployeeException("The specified " + expectedType.toString() + " has an invalid type.");
+    }
+
+    public Report findById(Long id) {
+        return reportRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Report cannot be found."));
     }
 
     public List<Report> findAll() {
