35f0abd1be235c5408a6105b5bd60316158aeecb
Thales Senra <thalesmarinhotwn@hotmail.com>
2025-03-04 18:46:17 -0300

    feat: add InvalidEmployeeException.java and move validation from NoteMapper.java to NoteService.java

--- src/main/java/com/vasconcellos/dailyreport/mapper/NoteMapper.java
+++ src/main/java/com/vasconcellos/dailyreport/mapper/NoteMapper.java
@@ -1,7 +1,6 @@
 package com.vasconcellos.dailyreport.mapper;
 
 import com.vasconcellos.dailyreport.dto.NoteDto;
-import com.vasconcellos.dailyreport.exception.InvalidDateException;
 import com.vasconcellos.dailyreport.model.Employee;
 import com.vasconcellos.dailyreport.model.Note;
 import com.vasconcellos.dailyreport.model.Order;
@@ -9,7 +8,6 @@ import com.vasconcellos.dailyreport.model.Report;
 import com.vasconcellos.dailyreport.service.EmployeeService;
 import com.vasconcellos.dailyreport.service.OrderService;
 import com.vasconcellos.dailyreport.service.ReportService;
-import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
@@ -35,24 +33,18 @@ public abstract class NoteMapper {
     @Mapping(source = "order.id", target = "orderId")
     public abstract NoteDto toDto(Note note);
 
-    @BeforeMapping
-    public void validateNote(NoteDto data) {
-        if(data.getExitTime().isBefore(data.getEntryTime()))
-            throw new InvalidDateException("Exit date cannot be before entry date.");
-    }
-
     @Named("mapReportById")
     public Report mapReportById(Long id) {
-        return id != null ? reportService.findById(id).orElse(null) : null;
+        return reportService.findById(id);
     }
 
     @Named("mapEmployeeById")
     public Employee mapEmployeeById(Long id) {
-        return id != null ? employeeService.findById(id).orElse(null) : null;
+        return employeeService.findById(id);
     }
 
     @Named("mapOrderById")
     public Order mapOrderById(Long id) {
-        return id != null ? orderService.findById(id).orElse(null) : null;
+        return orderService.findById(id);
     }
 }
--- src/main/java/com/vasconcellos/dailyreport/service/NoteService.java
+++ src/main/java/com/vasconcellos/dailyreport/service/NoteService.java
@@ -1,7 +1,10 @@
 package com.vasconcellos.dailyreport.service;
 
 import com.vasconcellos.dailyreport.dto.NoteDto;
+import com.vasconcellos.dailyreport.exception.InvalidDateException;
+import com.vasconcellos.dailyreport.exception.ResourceNotFoundException;
 import com.vasconcellos.dailyreport.mapper.NoteMapper;
+import com.vasconcellos.dailyreport.model.Employee;
 import com.vasconcellos.dailyreport.model.Note;
 import com.vasconcellos.dailyreport.repository.NoteRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +12,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -22,15 +24,26 @@ public class NoteService {
     public Note save(NoteDto data) {
         Note toSave = noteMapper.toEntity(data);
 
+        for(Note existingNote : findByEmployee(toSave.getEmployee()))
+            if (existingNote.isOverlapping(toSave))
+                throw new InvalidDateException("The exit time cannot overlap with an existing entry time.");
+
+        if(data.getExitTime().isBefore(data.getEntryTime()))
+            throw new InvalidDateException("Exit date cannot be before entry date.");
+
         return noteRepository.save(toSave);
     }
 
-    public Optional<Note> findById(Long id) {
-        return noteRepository.findById(id);
+    public Note findById(Long id) {
+        return noteRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Note cannot be found."));
     }
 
     public List<Note> findAll() {
         return noteRepository.findAll();
+    }
+
+    public List<Note> findByEmployee(Employee employee) {
+        return noteRepository.findByEmployee(employee);
     }
 
     public void deleteById(Long id) {
