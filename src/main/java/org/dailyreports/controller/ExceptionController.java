38982c4e6eb6ae2b8c5c577711a5ed94d37d5170
Thales Senra <thalesmarinhotwn@hotmail.com>
2025-03-21 08:48:11 -0300

    fix: change token validation exception response to not_found

--- src/main/java/org/dailyreports/controller/ExceptionController.java
+++ src/main/java/org/dailyreports/controller/ExceptionController.java
@@ -6,6 +6,7 @@ import org.dailyreports.exception.ResourceNotFoundException;
 import org.dailyreports.exception.TokenValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -30,6 +31,11 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {TokenValidationException.class})
     private ResponseEntity<String> handleTokenValidationException(TokenValidationException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = {BadCredentialsException.class})
+    private ResponseEntity<String> handleBadCredentialsException(BadCredentialsException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
--- src/main/java/org/dailyreports/exception/TokenValidationException.java
+++ src/main/java/org/dailyreports/exception/TokenValidationException.java
@@ -3,7 +3,7 @@ package org.dailyreports.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class TokenValidationException extends RuntimeException {
 
     public TokenValidationException(String message) {
